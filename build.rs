40e93890820822372b98fded965108755555a6fa
Miles Granger <miles59923@gmail.com>
2024-09-24 11:06:48 +0200

    Update feature gates [skip ci]

--- build.rs
+++ build.rs
@@ -1,15 +1,6 @@
 fn main() {
     // See note in Cargo.toml
-    if cfg!(target_pointer_width = "64") {
-        #[cfg(feature = "use-system-isal")]
-        println!("cargo:rustc-cfg=feature=\"isal-rs/use-system-isal\"");
-
-        #[cfg(feature = "isal-static")]
-        println!("cargo:rustc-cfg=feature=\"isal-rs/static\"");
-
-        #[cfg(feature = "isal-shared")]
-        println!("cargo:rustc-cfg=feature=\"isal-rs/shared\"");
-    } else {
+    if cfg!(target_pointer_width = "32") {
         let msg = "feature set but ISA-L not supported on 32 bit systems.";
 
         #[cfg(feature = "use-system-isal")]
